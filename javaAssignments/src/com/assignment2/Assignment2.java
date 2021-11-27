afac92c4a86521fde2df0ed66eb8c17072e7bddf
Dayaker <dayakerreddy.kandala@zemosolabs.com>
2021-11-27 22:23:36 +0530

    2nd commit

--- javaAssignments/src/com/assignment2/Assignment2.java
+++ javaAssignments/src/com/assignment2/Assignment2.java
@@ -23,6 +23,25 @@ public class Assignment2 {
         else
             System.out.println("The String does not contain all the letters of the alphabets ");
 
+        /*
+        import java.util.Scanner;
+        import java.util.Set;
+        import java.util.HashSet;
+        public class Assignment2 {
+            public static void main(String args[]) {
+                Scanner scanner = new Scanner(System.in);
+                String inputString = scanner.nextLine().toLowerCase();
+                Set<Character> st = new HashSet<>();
+
+                for(char ch : inputString.toCharArray()){
+                    if(ch >= 'a' && ch<='z')
+                        st.add(ch);
+                }
+                System.out.println(st.size() == 26);
+           }
+        }
+         */
+
     }
 }
 //Time complexity is O(n) where n is string length
